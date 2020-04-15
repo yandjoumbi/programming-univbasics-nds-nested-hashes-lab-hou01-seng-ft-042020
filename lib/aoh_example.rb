ee1bf9790f0c238dc29b2650c79de7fcdc26ecf0
Yannick <ydjoumbi@gmail.com>
2020-04-15 01:32:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -29,10 +29,13 @@ end
 
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  
+  
 end
